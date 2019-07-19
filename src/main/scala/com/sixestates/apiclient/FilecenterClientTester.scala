2fc5c787c5a33c2838aa8071d64e8f64a25c9fdb
邱明明 <qiumingming@qiumingmingdeMacBook-Pro.local>
2019-07-19 12:41:32 +0800

    fix test class

--- src/main/scala/com/sixestates/apiclient/FilecenterClientTester.scala
+++ src/main/scala/com/sixestates/apiclient/FilecenterClientTester.scala
@@ -3,14 +3,20 @@ package com.sixestates.apiclient
 import java.io.BufferedReader
 import java.util
 
+import com.alibaba.fastjson
+import com.alibaba.fastjson.JSON
 import com.lakeside.core.ArgOptions
 import com.sixestates.apiclient.filecenter.check.ThuKeywordFileDataChecker
+import com.sixestates.apiclient.filecenter.client.{FileCenterClient, FileCenterClientFactory}
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
+import org.slf4j.{Logger, LoggerFactory}
 
-import scala.util.parsing.json.JSONObject;
+import scala.util.parsing.json.JSONObject
+
 
 object FilecenterClientTester {
-  protected val log : Logger = LoggerFactory.getLogger(FilecenterClientTester.class);
+  protected val log : Logger = LoggerFactory.getLogger(FilecenterClientTester.getClass)
   protected val fileCenterClient: FileCenterClient = FileCenterClientFactory.getInstance("common", "ceph_gateway", "sg-west", "qiumingming@6estates.com")
 
   def main(args: Array[String]): Unit = {
@@ -32,45 +38,54 @@ object FilecenterClientTester {
 
     val sc = new SparkContext(conf);
 
-    val lines = getFileRdd(9L)
+    val lines : List[String] = getFileRdd(9L)
 
-    var rdd = sc.parallelize(lines)
+    val rdd : RDD[String] = sc.parallelize(lines)
 
+    val wordRdd = rdd.filter(line => line!=null && !line.isEmpty)
+      .flatMap(wordLine => {
+        wordLine.split(" ")
+      })
 
-    val checker = new ThuKeywordFileDataChecker();
-    checker.dataCheck(9L);
+    val sortWordRdd = wordRdd.map(word => {
+      (word, 1)
+    }).reduceByKey((a, b) => a + b).sortBy(
+      _._2, false
+    ).take(100)
+
+    sortWordRdd.foreach(wc =>{
+      println()
+    })
 
   }
 
-  def getFileRdd(fileId : Long): List<String> = {
-    val lines : List<String> = new util.ArrayList[]()
+  def getFileRdd(fileId : Long): List[String] = {
+    val lines : List[String] = List()
     val bufferedReader = this.fileCenterClient.bufferedReadFile(fileId)
     var order = 1
     var errorSize = 0
 
     var line = bufferedReader.readLine
-    while ( {
-      StringUtils.isNotEmpty(line)
-    }) {
-      val tokens = this.doCheck(line)
+    while (line != null && !line.isEmpty) {
+      val tokens = doCheck(line)
       if (tokens == null) {
         log.error("Data[{}] format error : {}", order, line)
         errorSize = errorSize + 1
       }else{
-        lines.add(line);
+        lines.+(line)
       }
-      if (order % 10000 == 0) log.info("Now check data size {}/{}, and find error data size : {}", Array[AnyRef](order, lineSize, errorSize))
+      if (order % 10000 == 0) log.info("Now check data size {}/{}, and find error data size : {}", order, lines.count(s => true), errorSize)
       line = bufferedReader.readLine
       order = order + 1
     }
 
     bufferedReader.close();
-    log.info("Totally check data size {}/{}, and find error data size : {}", Array[AnyRef](order, lineSize, errorSize))
+    log.info("Totally check data size {}/{}, and find error data size : {}", order, lines.count(s => true), errorSize)
     return lines;
   }
 
-  def docheck(line : String) : String = {
-        val jo: JSONObject = JSONObject.parseObject(line);
+  def doCheck(line : String) : String = {
+        val jo: fastjson.JSONObject = JSON.parseObject(line)
         if (jo.containsKey("_tokens") && jo.containsKey("cmi_id")) {
           return jo.getString("_tokens");
         } else {
